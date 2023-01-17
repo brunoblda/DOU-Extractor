23178030dff324b79a0104f59163b4d4ebc46034
Bruno Luiz de Deus Adão <brunoblda@gmail.com>
2023-01-17 00:06:40 -0300

    arquivo bat

--- dou-extract.bat
+++ dou-extract.bat
@@ -1,4 +1,13 @@
 @ECHO OFF 
-REM This is a commentary
 :: Another commentary
-ECHO "hello"
+set dateToday = %Date%
+set yearToday = %dateToday~6%
+set monthToday = %dateToday~3,2%
+set dayToday = %dateToday~0,2%
+for /r %%i in (%1) do (
+  set fileName=%%i
+  set yearFile=%fileName~10,4%
+  set monthFile=%fileName~15,2%
+  set dayFile=%fileName~18,2%
+  if yearFile == 
+)
--- main.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-URL = "http://diariooficialhoje.com.br/IN-Jornal-2023_01_10_do2.pdf"
-response = requests.get(URL)
-open("DOU-2023-01-23.pdf", "wb").write(response.content)
