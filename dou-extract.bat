c06a0eff6ecd70dbb3a5b358e20968da2d5ca41d
brunoblda <bruno.adao@iphan.gov>
2023-01-17 19:09:57 -0300

    incluindo o curl

--- dou-extract.bat
+++ dou-extract.bat
@@ -1,13 +1,15 @@
 @ECHO OFF 
 :: Another commentary
-set dateToday = %Date%
-set yearToday = %dateToday~6%
-set monthToday = %dateToday~3,2%
-set dayToday = %dateToday~0,2%
+set dateToday=%Date%
+set yearToday=%dateToday~6%
+set monthToday=%dateToday~3,2%
+set dayToday=%dateToday~0,2%
+set count=0
 for /r %%i in (%1) do (
   set fileName=%%i
   set yearFile=%fileName~10,4%
   set monthFile=%fileName~15,2%
   set dayFile=%fileName~18,2%
-  if yearFile == 
+  if (yearFile==yearToday AND monthFile==monthToday AND dayFile==dayToday) count+=1     
 )
+if count==0 
