f309b3aa1522640563139bd21c87e507783eeb24
brunoblda <bruno.adao@iphan.gov>
2023-01-18 19:32:26 -0300

    get date

--- dou-extract.bat
+++ dou-extract.bat
@@ -1,15 +1,28 @@
 @ECHO OFF 
-:: Another commentary
-set dateToday=%Date%
-set yearToday=%dateToday~6%
-set monthToday=%dateToday~3,2%
-set dayToday=%dateToday~0,2%
+:: %1 = pasta para salvar
+
+For /f "tokens=1-6 delims=/: " %%A in ('robocopy "|" . /njh ^| find ":"') do  (
+    set "_YYYY=%%A" & set "_month=%%B" & set "_day=%%C" 
+    set "_hour=%%D" & set "_min=%%E"   & set "_sec=%%F"
+)
+
+set yearToday=%_YYYY%
+set monthToday=%_month%
+set dayToday=%_day%
 set count=0
+
+set downloadFileName=IN-Jornal-%yearToday%_%monthToday%_%dayToday%_do2.pdf
+set fileResult=%1%downloadFileName%
+ECHO %fileResult%
+
 for /r %%i in (%1) do (
   set fileName=%%i
   set yearFile=%fileName~10,4%
   set monthFile=%fileName~15,2%
   set dayFile=%fileName~18,2%
-  if (yearFile==yearToday AND monthFile==monthToday AND dayFile==dayToday) count+=1     
+  if (%yearFile%==%yearToday%) count+=1   
 )
-if count==0 
+set downloadFileName=IN-Jornal-%yearToday%_%monthToday%_%dayToday%_do2.pdf
+set fileResult=%1%downloadFileName%
+ECHO %fileResult%
+::if count==0 curl http://diariooficialhoje.com.br/IN-Jornal-2023_01_16_do2.pdf --output %1
